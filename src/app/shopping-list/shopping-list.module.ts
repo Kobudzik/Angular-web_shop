7b97d04eaba8fafa2603c2ac60a2cf55a8936a33
Kobudzik <kobudzik98@gmail.com>
2020-09-03 17:50:21 +0200

    Implemented lazy loading

---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,26 @@
+import { SharedModule } from './../shared/shared.module';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping-list.component';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    declarations: [
+        ShoppingListComponent,
+        ShoppingEditComponent,
+    ],
+    imports: [
+        SharedModule,
+        FormsModule,
+        RouterModule.forChild([
+            { path: '', component: ShoppingListComponent }
+        ])
+
+
+    ],
+
+})
+
+export class ShoppingListModule{
+}
