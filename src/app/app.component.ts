ef4511bbebd5c19110da3afc05f14565e2d63599
Kobudzik <kobudzik98@gmail.com>
2020-09-16 08:48:25 +0200

    Instaled modules, implemented ng universal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { AuthService } from './auth/auth.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID, inject } from '@angular/core';
 import * as fromApp from './store/app.reducer';
 import * as AuthActions from './auth/store/auth.actions';
 import { Store } from '@ngrx/store';
+import { isPlatformBrowser } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,9 +13,13 @@ import { Store } from '@ngrx/store';
 export class AppComponent implements OnInit {
   constructor(
     private authService: AuthService,
-    private store: Store<fromApp.AppState>){}
+    private store: Store<fromApp.AppState>,
+    @Inject(PLATFORM_ID) private platformId
+  ){}
 
   ngOnInit(): void {
-    this.store.dispatch(new AuthActions.AutoLogin());
+    if (isPlatformBrowser(this.platformId)){
+      this.store.dispatch(new AuthActions.AutoLogin());
+    }
   }
 }
